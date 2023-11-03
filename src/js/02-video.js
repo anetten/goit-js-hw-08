c82a942300ddb921aafcc3c9d1600b7191cbd9e4
anetten <anet.zvekova@gmail.com>
2023-11-03 18:53:42 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+function getItemStorage() {
+  const savedTime = localStorage.getItem(STORAGE_KEY);
+  return savedTime;
+}
+
+player.on(
+  'timeupdate',
+  throttle(evt => {
+    const currentTime = evt.seconds;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(currentTime));
+  }, 1000)
+);
+
+if (getItemStorage() > 0) {
+  player.setCurrentTime(getItemStorage());
+}
